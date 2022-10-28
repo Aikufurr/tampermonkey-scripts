b5bbf52c765576db55c568c052b4fe1cdf576487
Aikufurr <ikeybill1@gmail.com>
2022-10-28 22:20:49 +0100

    Create Let_me_download.user.js

---
+++ Let_me_download.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Let me download
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.instagram.com/*
+// @icon         https://www.google.com/s2/favicons?domain=instagram.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    function checkDOMChange() {
+        [...document.getElementsByTagName('img')].filter(e=>e.src.startsWith("https://instagram.")).filter(e=>e.parentElement.tagName=="DIV").forEach(e => {try{if (e.parentElement.parentElement.children[1].tagName=="DIV") {e.parentElement.parentElement.children[1].remove()}}catch(_){}})
+        setTimeout( checkDOMChange, 1000 );
+    }
+    checkDOMChange();
+})();
